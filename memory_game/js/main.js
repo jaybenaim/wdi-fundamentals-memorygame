1aaac0eff62c69bfda804871367fea9dc6801b48
Jay Benaim <benaimjacob@gmail.com>
2019-06-29 01:51:45 -0400

    added objects assignment

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,29 @@
 
 
-var cards = ["queen", "queen", "king", "king"]; 
+var cards = [
+    { 
+        rank : "queen", 
+        suit: "hearts", 
+        cardImage: "images/queen-of-hearts.png"
+
+},
+    { 
+        rank : "queen", 
+        suit: "diamonds", 
+        cardImage: "images/queen-of-diamonds.png"
+
+}, 
+    { 
+        rank : "king", 
+        suit: "hearts", 
+        cardImage: "images/king-of-hearts.png"
+}, 
+    { 
+        rank : "king", 
+        suit: "diamonds", 
+        cardImage: "images/king-of-diamonds.png"
+}
+]; 
 var cardsInPlay = []; 
 
 const checkForMatch = () => { 
@@ -9,19 +32,26 @@ const checkForMatch = () => {
     } else {  
        return "Not a match!"; 
     }
- }
+ }; 
+ 
+// get the cards by the index number 
 const flipCard = (cardId) => { 
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    var match = checkForMatch();
+    
     if (cardsInPlay.length === 2) { 
-        var match = checkForMatch();
+        return  "User flipped " + cards[cardId].rank + " " + match;
     }  else { 
-        return "User flipped " + cards[cardId]; 
+        return "User flipped " + cards[cardId].rank;
     }
-    // get the cards by the index number 
-    return  "User flipped " + cards[cardId] + " " + match ;
+    
      
-}
-console.log(flipCard(3)); 
+};
+
+console.log(flipCard(0)); 
 console.log(flipCard(1)) ; 
+
 // when a user flips a card add that card to array of cards in play 
 // if the user flipped two cards check for a match 
--- memory_game/test.js
+++ memory_game/test.js
@@ -1,17 +1,95 @@
-// write your code below
-var result; 
-var max = 20; 
-
-
-for(let i = 1; i <= max; i++ ) {  
-if(i % 3 === 0 ) { 
-  result = "fizz"; 
-} else if(i % 5 === 0) { 
-  result = "buzz"; 
-} else if ( i % 3 === 0 && i % 5 === 0) { 
-  result = "fizzbuzz"; 
-} else { 
-  result = i;  
-} 
- console.log(result); 
-}
+var grungeAlbums = {
+    "albums":[
+      {
+          "name": "Bleach",
+          "artist": "Nirvana",
+          "unitsSold": 1700000
+      },
+      {
+          "name": "Nevermind",
+          "artist": "Nirvana",
+          "unitsSold": 30000000
+      },
+      {
+          "name": "In Utero",
+          "artist": "Nirvana",
+          "unitsSold": 15000000
+      },
+      {
+          "name": "Ten",
+          "artist": "Pearl Jam",
+          "unitsSold": 10000000
+      },
+      {
+          "name": "Vs",
+          "artist": "Pearl Jam",
+          "unitsSold": 6100000
+      },
+      {
+          "name": "Vitalogy",
+          "artist": "Pearl Jam",
+          "unitsSold": 4770000
+      }
+    ]
+  };
+  
+  var grungeAlbumsJSON = JSON.stringify(grungeAlbums); 
+  var grungeAlbumsObject = JSON.parse(grungeAlbumsJSON); 
+  
+//   console.log(grungeAlbumsJSON)
+//   console.log(grungeAlbumsObject)
+  
+  
+  for(let i = 0; i < grungeAlbumsObject.length; i++) { 
+    console.log(grungeAlbumsObject[i].albums.name + grungeAlbumsObject[i].albums.artist + grungeAlbumsObject[i].albums.unitsSold)
+  }
+  
+  var grungeAlbums = {
+    "albums":[
+      {
+          "name": "Bleach",
+          "artist": "Nirvana",
+          "unitsSold": 1700000
+      },
+      {
+          "name": "Nevermind",
+          "artist": "Nirvana",
+          "unitsSold": 30000000
+      },
+      {
+          "name": "In Utero",
+          "artist": "Nirvana",
+          "unitsSold": 15000000
+      },
+      {
+          "name": "Ten",
+          "artist": "Pearl Jam",
+          "unitsSold": 10000000
+      },
+      {
+          "name": "Vs",
+          "artist": "Pearl Jam",
+          "unitsSold": 6100000
+      },
+      {
+          "name": "Vitalogy",
+          "artist": "Pearl Jam",
+          "unitsSold": 4770000
+      }
+    ]
+  };
+  
+  var grungeAlbumsJSON = JSON.stringify(grungeAlbums); 
+  var grungeAlbumsObject = JSON.parse(grungeAlbumsJSON); 
+  
+//   console.log(grungeAlbumsJSON); 
+//   console.log(grungeAlbumsObject); 
+  
+function make() { 
+  
+  for(let i = 0; i < grungeAlbumsObject.albums.length; i++) { 
+      const currentAlbum = grungeAlbumsObject.albums[i]; 
+    console.log("Album: " + currentAlbum.name + " Artist: " +  currentAlbum.artist +  " Units Sold: " + currentAlbum.unitsSold)
+  }
+  }
+  make() ; 
