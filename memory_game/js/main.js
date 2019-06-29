f4b5567065a65e3468bc051bea0d6305163fcfc9
Jay Benaim <benaimjacob@gmail.com>
2019-06-28 23:47:57 -0400

    added flipCard and CheckForMatch functions to main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,26 @@
 
 var cards = ["queen", "queen", "king", "king"]; 
 var cardsInPlay = []; 
-var cardOne = cards[0]; 
-var cardTwo = cards[2]; 
 
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo); 
-
-
-
-if(cardsInPlay.length === 2) { 
+const checkForMatch = () => { 
     if(cardsInPlay[0] === cardsInPlay[1]) { 
-        alert("you found a match"); 
-        console.log("User flipped" + cardsInPlay); 
+        return "You found a match!"; 
     } else {  
-        console.log("not a match"); 
+       return "Not a match!"; 
     }
+ }
+const flipCard = (cardId) => { 
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) { 
+        var match = checkForMatch();
+    }  else { 
+        return "User flipped " + cards[cardId]; 
+    }
+    // get the cards by the index number 
+    return  "User flipped " + cards[cardId] + " " + match ;
+     
 }
+console.log(flipCard(3)); 
+console.log(flipCard(1)) ; 
+// when a user flips a card add that card to array of cards in play 
+// if the user flipped two cards check for a match 
