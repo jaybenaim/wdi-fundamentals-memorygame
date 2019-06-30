1137796e3576a8d9b17d4129ddd1290a1cd00b35
Jay Benaim <benaimjacob@gmail.com>
2019-06-30 14:52:32 -0400

    added comments

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,13 +3,11 @@ var cards = [
         rank : "queen", 
         suit: "hearts", 
         cardImage: "images/queen-of-hearts.png"
-
 },
     { 
         rank : "queen", 
         suit: "diamonds", 
         cardImage: "images/queen-of-diamonds.png"
-
 }, 
     { 
         rank : "king", 
@@ -21,20 +19,13 @@ var cards = [
         suit: "diamonds", 
         cardImage: "images/king-of-diamonds.png"
 }
-]; 
+];
+
 var cardsInPlay = []; 
 var score = 0; 
  
-
-function storeScore() {
-    localStorage.setItem('score', Number(score) );
-    let currentScore = localStorage.getItem('score');
-    // display score  
-    $('#score').html(''); 
-    $('#score').append(currentScore); 
-}
 const checkForMatch = () => { 
-    //&& cardsInPlay.length === 2 || cardsInPlay.length === 4
+    // Check if two cards matched 
     if(cardsInPlay[0] === cardsInPlay[1]) { 
         // delay the alert 
         setTimeout(function(){
@@ -43,49 +34,61 @@ const checkForMatch = () => {
         // add 1 to the score when a match is made 
         Number(score++);
        storeScore(); 
+       // clear clicked cards array 
        cardsInPlay = []; 
-    } else if (cardsInPlay[0] !== cardsInPlay[1] ) {  
-        setTimeout(function(){
-            alert("Not a match!");
-         }, 200);
-         storeScore(); 
-         Number(score) = 0;   
-         cardsInPlay = [];
-    } else if (cardsInPlay.length === 2 || cardsInPlay.length === 4) { 
-        score.replace(score, currentScore); 
-
     } else { 
         setTimeout(function(){
             alert("Not a match!");
          }, 200);
-         storeScore(); 
-         $('#score').hide(); 
-
+        storeScore(); 
+        cardsInPlay = [];
     }
  }; 
 
 var flipCard = function() {
-	var cardId = this.getAttribute('data-id');
-	this.setAttribute('src', cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
+    // set cards[cardId] using index number 
+    var cardId = this.getAttribute('data-id');
+    // add clicked cards image 
+    this.setAttribute('src', cards[cardId].cardImage);
+    // add clicked cards rank
+    cardsInPlay.push(cards[cardId].rank);
+    // check if there are two cards selected 
 	if(cardsInPlay.length === 2 ) {
+        // check if the cards match 
         checkForMatch();
     } 
 }
 
 const createBoard = () => { 
     for(let i = 0; i < cards.length; i++) { 
+        // create card images 
         var cardElement = document.createElement("img"); 
+        // set each cards image src to back.png image 
         cardElement.setAttribute('src', "images/back.png"); 
+        // set each cards data-id attribute 
         cardElement.setAttribute('data-id', i); 
+        // draw each image card onto the html in the game board div 
         document.getElementById('game-board').appendChild(cardElement); 
+        // add click event to each card 
         cardElement.addEventListener('click', flipCard);
     }
 }
 
-// refresh the page on reset button click 
-function Refresh() {
-     window.parent.location = window.parent.location.href;
+function storeScore() {
+    // set a local storage with a score variable 
+    localStorage.setItem('score', Number(score));
+    // get the stored score and store it to a variable 
+    let currentScore = localStorage.getItem('score');
+    // clear previous score  
+    $('#score').html(''); 
+    // add new score to score id <p> in index.html
+    $('#score').append(currentScore); 
 }
 
+// refresh the page on reset button click 
+function Refresh() {
+    // refresh the page when clicking the reset button 
+    window.parent.location = window.parent.location.href;
+}
+// create the cards and display them on the webpage
 createBoard(); 
